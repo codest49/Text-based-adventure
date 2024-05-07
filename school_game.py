7c37fba5c1f4f167260913ddcf0d9b3c900e87b7
codest49 <131542092+codest49@users.noreply.github.com>
2024-05-07 12:29:39 +0000

    r

--- school_game.py
+++ school_game.py
@@ -1,12 +1,12 @@
-#for later
-#invontory=
+invontory= ''
 def r0_1():
     print('')
 def r0_3():
     print('') 
 def r1_1():
-    print('')
-print
+    print('you found money on the ground. \nWhat do you do?: \npick it up \nburn it \nleave it')
+    if input()=="pick it up":
+        print()
 print('welcome to my game!\nType start to begin') 
 if input()=="start":
     print('make your choice on which way to go. \nLeft or right')
