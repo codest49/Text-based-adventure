addb447d20e33a106715e92896350bd091548bb1
codest49 <131542092+codest49@users.noreply.github.com>
2024-05-07 12:16:48 +0000

    r

--- school_game.py
+++ school_game.py
@@ -3,8 +3,14 @@
 def r0_1():
     print('')
 def r0_3():
-    print()
+    print('') 
+def r1_1():
+    print('')
 print
-print('welcome to my game!\ntype start to begin') 
+print('welcome to my game!\nType start to begin') 
 if input()=="start":
+    print('make your choice on which way to go. \nLeft or right')
+if input()=='left':
+    r1_1()
+if input()=='right':
     r0_1()
