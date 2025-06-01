93c30234ca4947e832893f81da62d77d9fe8a70f
R <codest49@gmail.com>
2025-06-01 18:54:32 -0400

    setup pygame

--- game.py
+++ game.py
@@ -1,2 +1,8 @@
 #print("you encountered a wild mass murderer!")
-import pygame
+import pygame 
+
+#pygame setup
+pygame.init()
+screen=pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+running = True
